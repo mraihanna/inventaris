4ef65a01fcaa5430d618794f461bda7bf855aa76
mraihanna <mraihanna1278@gmail.com>
2022-09-05 00:57:40 +0700

    update new kib c

--- application/controllers/Aset.php
+++ application/controllers/Aset.php
@@ -84,10 +84,17 @@ class Aset extends CI_Controller
     $data['sumber_barang']  = $this->db->get('sumber_barang')->result_array();
 
     $this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required');
-    $this->form_validation->set_rules('luas', 'Luas', 'required');
-    $this->form_validation->set_rules('tahun', 'Tahun', 'required|max_length[20]');
-    $this->form_validation->set_rules('nomor_sertifikat', 'Nomor Sertifikat', 'required');
+    $this->form_validation->set_rules('merk', 'Type atau Merk', 'required');
+    $this->form_validation->set_rules('bahan', 'Bahan', 'required');
+    $this->form_validation->set_rules('kondisi', 'Kondisi', 'required');
+    $this->form_validation->set_rules('kategori', 'Kategori', 'required');
+    $this->form_validation->set_rules('ruangan', 'Ruangan', 'required');
+    $this->form_validation->set_rules('tgl', 'Tanggal Pengadaan', 'required');
+    $this->form_validation->set_rules('umur_ekonomis', 'Umur Ekonomis', 'required');
+    $this->form_validation->set_rules('jumlah', 'Jumlah', 'required');
     $this->form_validation->set_rules('harga', 'Harga', 'required');
+    $this->form_validation->set_rules('semua_barang', 'Semua Barang', 'required');
+    $this->form_validation->set_rules('bahan', 'Bahan ', 'required');
 
     if ($this->form_validation->run() == false) {
       $this->load->view('templates/header', $data);
@@ -96,22 +103,47 @@ class Aset extends CI_Controller
       $this->load->view('aset/kib-b', $data);
       $this->load->view('templates/footer');
     } else {
-      $count      = $this->db->get('kib_b')->num_rows();
-      $count      = $count + 1;
-      $id         = str_pad($count, 5, "0", STR_PAD_LEFT);
-      $nomor_aset = 'KIB/A/12.01.10.15.08.01/69/' . $this->input->post('tahun') . '/' . $id;
+      $jumlah = $this->input->post('jumlah');
+      for ($i = 0; $i < $jumlah; $i++) {
 
-      $data = [
-        'nomor_aset'        => $nomor_aset,
-        'nama_barang'       => $this->input->post('nama_barang'),
-        'luas'              => $this->input->post('luas'),
-        'tahun'             => $this->input->post('tahun'),
-        'nomor_sertifikat'  => $this->input->post('nomor_sertifikat'),
-        'harga'             => $this->input->post('harga'),
-      ];
+        $tgl = strtotime($this->input->post('tgl'));
+        $tahun =  date('Y', $tgl);
 
-      $this->db->insert('kib_b', $data);
+        $count      = $this->db->get('kib_b')->num_rows();
+        $count      = $count + 1;
+        $id         = str_pad($count, 5, "0", STR_PAD_LEFT);
+        $nomor_aset = 'KIB/B/12.01.10.15.08.01/69/' . $this->input->post('kategori') . '/' . $this->input->post('ruangan') . '/' . $tahun . '/' . $id;
 
+        $sumber_barang = $this->db->get_where('sumber_barang', ['id' => $this->input->post('semua_barang')])->row_array();
+
+        if ($this->input->post('kondisi') == 1) {
+          $status_barang = "Deadstock/Tidak Bisa diperbaiki";
+        } elseif ($this->input->post('kondisi') == 2) {
+          $status_barang = "Bisa Diperbaiki/Direnovasi";
+        } else {
+          $status_barang = "-";
+        }
+        $data = [
+          'id_kondisi'          => $this->input->post('kondisi'),
+          'id_kategori'         => $this->input->post('kategori'),
+          'id_ruangan'          => $this->input->post('ruangan'),
+          'id_sumber_barang'    => $this->input->post('semua_barang'),
+          'nomor_aset'          => $nomor_aset,
+          'nama_barang'         => $this->input->post('nama_barang'),
+          'merk'                => $this->input->post('merk'),
+          'bahan'               => $this->input->post('bahan'),
+          'tanggal_pengadaan'   => $this->input->post('tgl'),
+          'tahun'               => $tahun,
+          'umur_ekonomis'       => $this->input->post('umur_ekonomis'),
+          'jumlah'              => $this->input->post('jumlah'),
+          'harga'               => $this->input->post('harga'),
+          'kode_sumber_barang'  => $sumber_barang['sumber_barang'] . '/' . $tahun,
+          'status_barang'       => $status_barang
+        ];
+
+        $this->db->insert('kib_b', $data);
+      }
+      // die;
       $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Kib B Has been added!</div>');
       redirect('aset/kib_b');
     }
@@ -119,19 +151,26 @@ class Aset extends CI_Controller
 
   public function editkib_b($id)
   {
-    $kibA = $this->db->get_where('kib_b', ['id' => $id])->row_array();
-    $nomorAsetOld = $kibA['nomor_aset'];
-    $tahunOld = $kibA['tahun'];
-    $tahunNew = $this->input->post('tahun');
-    $nomor_aset = str_replace("$tahunOld", "$tahunNew", "$nomorAsetOld");
+    $kibb = $this->db->get_where('kib_b', ['id' => $id])->row_array();
+    $nomor_aset = $kibb['nomor_aset'];
+    $ruanganOld = '/' . $kibb['id_ruangan'] . '/';
+    $ruanganNew = '/' . $this->input->post('ruangan') . '/';
+
+    $nomor_aset = str_replace("$ruanganOld", "$ruanganNew", "$nomor_aset");
+
+    if ($this->input->post('kondisi') == 1) {
+      $status_barang = "Deadstock/Tidak Bisa diperbaiki";
+    } elseif ($this->input->post('kondisi') == 2) {
+      $status_barang = "Bisa Diperbaiki/Direnovasi";
+    } else {
+      $status_barang = "-";
+    }
 
     $data = [
       'nomor_aset'        => $nomor_aset,
-      'nama_barang'       => $this->input->post('nama_barang'),
-      'luas'              => $this->input->post('luas'),
-      'tahun'             => $this->input->post('tahun'),
-      'nomor_sertifikat'  => $this->input->post('nomor_sertifikat'),
-      'harga'             => $this->input->post('harga'),
+      'id_kondisi'        => $this->input->post('kondisi'),
+      'id_ruangan'        => $this->input->post('ruangan'),
+      'status_barang'     => $status_barang
     ];
 
     $this->db->where('id', $id);
@@ -139,5 +178,91 @@ class Aset extends CI_Controller
 
     $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">KIB A has been changed!</div>');
     redirect('aset/kib_b');
+  }
+  public function kib_c()
+  {
+    $data['title']          = 'KIB C';
+    $data['user']           = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data['kib_c']          = $this->db->get('kib_c')->result_array();
+    $data['kondisi']        = $this->db->get('kondisi')->result_array();
+
+    $this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required');
+    $this->form_validation->set_rules('kondisi', 'Kondisi', 'required');
+    $this->form_validation->set_rules('luas', 'Luas', 'required');
+    $this->form_validation->set_rules('lokasi', 'Lokasi', 'required');
+    $this->form_validation->set_rules('tahun', 'Tahun', 'required');
+    $this->form_validation->set_rules('harga', 'Harga', 'required');
+    $this->form_validation->set_rules('umur_ekonomis', 'Umur Ekonomis', 'required');
+
+    if ($this->form_validation->run() == false) {
+      $this->load->view('templates/header', $data);
+      $this->load->view('templates/sidebar', $data);
+      $this->load->view('templates/topbar', $data);
+      $this->load->view('aset/kib-c', $data);
+      $this->load->view('templates/footer');
+    } else {
+
+      $count      = $this->db->get('kib_c')->num_rows();
+      $count      = $count + 1;
+      $id         = str_pad($count, 5, "0", STR_PAD_LEFT);
+      $nomor_aset = 'KIB/C/12.01.10.15.08.01/69/' . $this->input->post('tahun') . '/'  . $id;
+
+      if ($this->input->post('kondisi') == 1) {
+        $status_barang = "Perlu Renovasi Besar";
+      } elseif ($this->input->post('kondisi') == 2) {
+        $status_barang = "Perlu Perbaikan Ringan";
+      } else {
+        $status_barang = "-";
+      }
+      $data = [
+        'id_kondisi'          => $this->input->post('kondisi'),
+        'nomor_aset'          => $nomor_aset,
+        'nama_barang'         => $this->input->post('nama_barang'),
+        'luas'                => $this->input->post('luas'),
+        'lokasi'              => $this->input->post('lokasi'),
+        'tahun'               => $this->input->post('tahun'),
+        'harga'               => $this->input->post('harga'),
+        'umur_ekonomis'       => $this->input->post('umur_ekonomis'),
+        'status_barang'       => $status_barang
+      ];
+
+      $this->db->insert('kib_c', $data);
+
+      // die;
+      $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">KIB C Has been added!</div>');
+      redirect('aset/kib_c');
+    }
+  }
+
+  public function editkib_c($id)
+  {
+    if ($this->input->post('kondisi') == 1) {
+      $status_barang = "Perlu Renovasi Besar";
+    } elseif ($this->input->post('kondisi') == 2) {
+      $status_barang = "Perlu Perbaikan Ringan";
+    } else {
+      $status_barang = "-";
+    }
+
+    $kibC = $this->db->get_where('kib_c', ['id' => $id])->row_array();
+    $nomorAsetOld = $kibC['nomor_aset'];
+    $tahunOld = $kibC['tahun'];
+    $tahunNew = $this->input->post('tahun');
+    $nomor_aset = str_replace("$tahunOld", "$tahunNew", "$nomorAsetOld");
+
+    $data = [
+      'nomor_aset'        => $nomor_aset,
+      'tahun'             => $this->input->post('tahun'),
+      'id_kondisi'        => $this->input->post('kondisi'),
+      'luas'              => $this->input->post('luas'),
+      'harga'             => $this->input->post('harga'),
+      'status_barang'     => $status_barang
+    ];
+
+    $this->db->where('id', $id);
+    $this->db->update('kib_c', $data);
+
+    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">KIB B has been changed!</div>');
+    redirect('aset/kib_c');
   }
 }
--- application/views/aset/kib-c.php
+++ application/views/aset/kib-c.php
@@ -3,7 +3,7 @@
 
   <!-- Page Heading -->
   <h1 class="h3 text-gray-800"><?= $title; ?></h1>
-  <h4 class="text-gray-800 mb-4">Kartu Inventaris Barang Peralatan dan Mesin</h4>
+  <h4 class="text-gray-800 mb-4">Kartu Inventaris Barang Bangunan</h4>
 
 
   <div class="row">
@@ -15,7 +15,7 @@
       <?php endif; ?>
       <!-- yang atas diguynakan kalau error nya banyak form validationnya banayak -->
       <?= $this->session->flashdata('message'); ?>
-      <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newKibB">Add new KIB B</a>
+      <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newKibC">Add new KIB C</a>
       <!-- Hoverable Rows -->
       <div class="table-responsive">
         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -24,46 +24,30 @@
               <th scope="col">#</th>
               <th scope="col">Nomor Aset</th>
               <th scope="col">Nama Barang</th>
-              <th scope="col">Type/Merk</th>
-              <th scope="col">Bahan</th>
               <th scope="col">Kondisi</th>
-              <th scope="col">Kategori</th>
-              <th scope="col">Ruangan</th>
-              <th scope="col">Tanggal Pengadaan</th>
+              <th scope="col">Luas (m <sup>2</sup>)</th>
+              <th scope="col">Lokasi</th>
               <th scope="col">Tahun</th>
               <th scope="col">Umur Ekonomis</th>
               <th scope="col">Harga</th>
-              <th scope="col">Sumber Barang</th>
-              <th scope="col">Kode Sumber Barang</th>
               <th scope="col">Status Barang</th>
               <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
             <?php $i = 1; ?>
-            <?php foreach ($kib_b as $a) : ?>
+            <?php foreach ($kib_c as $a) : ?>
               <?php $kondisiById = $this->db->get_where('kondisi', ['id' => $a['id_kondisi']])->row_array(); ?>
-              <?php $kategoriById = $this->db->get_where('kategori', ['id' => $a['id_kategori']])->row_array(); ?>
-              <?php $ruanganById = $this->db->get_where('ruangan', ['id' => $a['id_ruangan']])->row_array(); ?>
-              <?php $sumber_barangById = $this->db->get_where('sumber_barang', ['id' => $a['id_sumber_barang']])->row_array(); ?>
-              <?php $tgl = strtotime($a['tanggal_pengadaan']); ?>
-              <?php $tanggal = date("d F Y", $tgl); ?>
-
               <tr>
                 <th scope="row"><?= $i; ?></th>
                 <td><?= $a['nomor_aset']; ?></td>
                 <td><?= $a['nama_barang']; ?></td>
-                <td><?= $a['merk']; ?></td>
-                <td><?= $a['bahan']; ?></td>
                 <td><?= $kondisiById['kondisi']; ?></td>
-                <td><?= $kategoriById['kategori']; ?></td>
-                <td><?= $ruanganById['ruangan']; ?></td>
-                <td><?= $tanggal; ?></td>
+                <td><?= $a['luas']; ?></td>
+                <td><?= $a['lokasi']; ?></td>
                 <td><?= $a['tahun']; ?></td>
                 <td><?= $a['umur_ekonomis']; ?></td>
                 <td>Rp. <?= number_format($a['harga']); ?></td>
-                <td><?= $sumber_barangById['sumber_barang']; ?></td>
-                <td><?= $a['kode_sumber_barang']; ?></td>
                 <?php if ($a['id_kondisi'] == 1) : ?>
                   <td class="text-danger"><?= $a['status_barang']; ?></td>
                 <?php elseif ($a['id_kondisi'] == 2) : ?>
@@ -72,7 +56,7 @@
                   <td><?= $a['status_barang']; ?></td>
                 <?php endif; ?>
                 <td>
-                  <a href="" class="badge badge-warning" title="EDIT" data-toggle="modal" data-target="#editKibB<?= $a['id']; ?>">Edit</a>
+                  <a href="" class="badge badge-warning" title="EDIT" data-toggle="modal" data-target="#editKibC<?= $a['id']; ?>">Edit</a>
                 </td>
               </tr>
               <?php $i++; ?>
@@ -93,16 +77,16 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="newKibB" tabindex="-1" role="dialog" aria-labelledby="newKibALabel" aria-hidden="true">
+<div class="modal fade" id="newKibC" tabindex="-1" role="dialog" aria-labelledby="newKibCLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="newKibBLabel">Add New KIB B</h5>
+        <h5 class="modal-title" id="newKibCLabel">Add New KIB C</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <form action="<?= base_url('aset/kib_b'); ?>" method="POST">
+      <form action="<?= base_url('aset/kib_c'); ?>" method="POST">
         <div class="modal-body">
           <div class="form-group">
             <label for="nomor_aset">Nomor Aset</label>
@@ -113,14 +97,6 @@
             <input type="text" class="form-control" id="nama_barang" name="nama_barang" placeholder="Nama Barang">
           </div>
           <div class="form-group">
-            <label for="nomor_aset">Type/Merk</label>
-            <input type="text" class="form-control" id="merk" name="merk" placeholder="Merk">
-          </div>
-          <div class="form-group">
-            <label for="nomor_aset">Bahan</label>
-            <input type="text" class="form-control" id="bahan" name="bahan" placeholder="bahan">
-          </div>
-          <div class="form-group">
             <label for="nomor_aset">Kondisi</label>
             <select class="form-control" name="kondisi" id="kondisi">
               <option value="">Pilih Kondisi</option>
@@ -129,48 +105,30 @@
               <?php endforeach; ?>
             </select>
           </div>
-          <div class="form-group">
-            <label for="nomor_aset">Kategori</label>
-            <select class="form-control" name="kategori" id="kategori">
-              <option value="">Pilih Kategori</option>
-              <?php foreach ($kategori as $k) : ?>
-                <option value="<?= $k['id']; ?>"><?= $k['kategori']; ?></option>
-              <?php endforeach; ?>
-            </select>
+          <label for="luas">Luas</label>
+          <div class="row">
+            <div class="form-group col-md-10">
+              <input type="text" class="form-control" id="luas" name="luas" placeholder="Luas">
+            </div>
+            <div class="form-group col-md-2">
+              <label for="luas" class="my-auto">m<sup>2</sup></label>
+            </div>
           </div>
           <div class="form-group">
-            <label for="nomor_aset">Ruangan</label>
-            <select class="form-control" name="ruangan" id="ruangan">
-              <option value="">Pilih Ruangan</option>
-              <?php foreach ($ruangan as $k) : ?>
-                <option value="<?= $k['id']; ?>"><?= $k['ruangan']; ?></option>
-              <?php endforeach; ?>
-            </select>
+            <label for="nomor_aset">Lokasi</label>
+            <input type="text" class="form-control" id="lokasi" name="lokasi" placeholder="Lokasi">
           </div>
           <div class="form-group">
-            <label for="nomor_aset">Tanggal Pengadaan</label>
-            <input type="date" class="form-control" id="tgl" name="tgl" placeholder="Tanggal Pengadaan">
-          </div>
-          <div class="form-group">
-            <label for="nomor_aset">Umur Ekonomis</label>
-            <input type="text" class="form-control" id="umur_ekonomis" name="umur_ekonomis" placeholder="Umur Ekonomis">
-          </div>
-          <div class="form-group">
-            <label for="nomor_aset">Jumlah</label>
-            <input type="text" class="form-control" id="jumlah" name="jumlah" placeholder="Jumlah">
+            <label for="nomor_aset">Tahun</label>
+            <input type="text" class="form-control" id="tahun" name="tahun" placeholder="Tahun">
           </div>
           <div class="form-group">
             <label for="nomor_aset">Harga</label>
             <input type="text" class="form-control" id="harga" name="harga" placeholder="Harga">
           </div>
           <div class="form-group">
-            <label for="nomor_aset">Sumber Barang</label>
-            <select class="form-control" name="semua_barang" id="semua_barang">
-              <option value="">Pilih Sumber Barang</option>
-              <?php foreach ($sumber_barang as $k) : ?>
-                <option value="<?= $k['id']; ?>"><?= $k['sumber_barang']; ?></option>
-              <?php endforeach; ?>
-            </select>
+            <label for="nomor_aset">Umur Ekonomis</label>
+            <input type="text" class="form-control" id="umur_ekonomis" name="umur_ekonomis" placeholder="Umur Ekonomis">
           </div>
         </div>
         <div class="modal-footer">
@@ -182,17 +140,17 @@
   </div>
 </div>
 
-<?php foreach ($kib_b as $a) : ?>
-  <div class="modal fade" id="editKibB<?= $a['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="editKibBLabel" aria-hidden="true">
+<?php foreach ($kib_c as $a) : ?>
+  <div class="modal fade" id="editKibC<?= $a['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="editKibCLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="editKibBLabel">Edit KIB B</h5>
+          <h5 class="modal-title" id="editKibCLabel">Edit KIB C</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <form action="<?= base_url('aset/editKib_b/') . $a['id']; ?>" method="POST">
+        <form action="<?= base_url('aset/editKib_c/') . $a['id']; ?>" method="POST">
           <div class="modal-body">
             <div class="form-group">
               <label for="nomor_aset">Nomor Aset</label>
@@ -210,13 +168,22 @@
                 <?php endforeach; ?>
               </select>
             </div>
+            <label for="luas">Luas</label>
+            <div class="row">
+              <div class="form-group col-md-10">
+                <input type="text" class="form-control" id="luas" name="luas" value="<?= $a['luas']; ?>" placeholder="Luas">
+              </div>
+              <div class="form-group col-md-2">
+                <label for="luas" class="my-auto">m<sup>2</sup></label>
+              </div>
+            </div>
             <div class="form-group">
-              <label for="nomor_aset">Ruangan</label>
-              <select class="form-control" name="ruangan" id="ruangan">
-                <?php foreach ($ruangan as $k) : ?>
-                  <option value="<?= $k['id']; ?>" <?= ($k['id'] == $a['id_ruangan']) ? "selected" : ""; ?>><?= $k['ruangan']; ?></option>
-                <?php endforeach; ?>
-              </select>
+              <label for="nomor_aset">Tahun</label>
+              <input type="text" class="form-control" id="tahun" name="tahun" placeholder="Tahun" value="<?= $a['tahun']; ?>">
+            </div>
+            <div class="form-group">
+              <label for="nomor_aset">Harga</label>
+              <input type="text" class="form-control" id="harga" name="harga" placeholder="Harga" value="<?= $a['harga']; ?>">
             </div>
           </div>
           <div class="modal-footer">
